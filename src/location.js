96d788b1c0cbd172f0c4170f245702ac4f447aa1
VitVintrlik <vitvintrlik@seznam.cz>
2020-09-11 19:58:17 +0200

    Get location onload finished again this its working

--- src/location.js
+++ src/location.js
@@ -1,19 +1,22 @@
-import  React, {Component} from "react";
+import  React, {Component, useState} from "react";
 
 class Location extends Component {
-
-    weatherObject = {
-        weather: null,
-        name: null,
-        country: null,
-        description: null,
-        temperature: null
-    }
     weatherApi = {
         key: "5f19c03a4c8370148cb0abfc7bf6ce8d",
         base: "https://api.openweathermap.org/data/2.5/"
     }
 
+    constructor() {
+        super();
+        this.state = {
+            weather: null,
+            name: null,
+            country: null,
+            description: null,
+            temperature: null
+        }
+        this.getLocation()
+    }
     dateBuilder = (d) => {
         let months = ["Leden", "Únor", "Březen", "Duben", "Květen", "Červen", "Červecen", "Srpen", "Září", "Říjen", "Listopad", "Prosinec"]
         let days = ["Sobota", "Pondělí", "Úterý", "Středa", "Čtvrtek", "Pátek", "Neděle"]
@@ -78,35 +81,40 @@ class Location extends Component {
         console.log(city)
         fetch(`${this.weatherApi.base}weather?q=${city}&units=metric&APPID=${this.weatherApi.key}&lang=cz`)
             .then(response => response.json()).then(json => {
-            this.weatherObject.weather = json
-            this.weatherObject.name = json.name
-            this.weatherObject.country = json.sys.country
-            this.weatherObject.description = json.weather[0].description
-            this.weatherObject.temperature = json.main.temp
+                this.setState({
+                    weather: json,
+                    name: json.name,
+                    country: json.sys.country,
+                    description: json.weather[0].description,
+                    temperature: json.main.temp
+                })
+            console.log("cityLoad")
         })
     }
 
+
+
+
     render() {
-        console.log("rendreing")
-        this.getLocation()
+
+        console.log("rendering")
         return (
             <div>
                 <div className="locationWrapper">
-                    <div className="location">{this.weatherObject.name}, {this.weatherObject.country}</div>
+                    <div className="location">{this.state.name}, {this.state.country}</div>
                     <div className="date">{this.dateBuilder(new Date())}</div>
                 </div>
                 <div className="weatherWrapper">
                     <div className="temperature">
-                        {Math.round(this.weatherObject.temperature)}°C
+                        {Math.round(this.state.temperature)}°C
                     </div>
                     <div className="weather">
-                        {this.weatherObject.description}
+                        {this.state.description}
                     </div>
                 </div>
             </div>
         )
     }
-
 }
 
 export default Location;
